babea05e9d93cafbc9fe7540d0dd5c7ff616357d
Debopam Biswas <debopambiswas36@gmail.com>
2019-11-05 16:48:52 +0530

    Added heroku config

--- server.js
+++ server.js
@@ -31,4 +31,9 @@ process.on('unhandledRejection', err => {
   });
 });
 
-
+process.on('SIGTERM', () => {
+  console.log('SIGTERM Received. Shutting down gracefully.');
+  server.close(() => {
+    console.log('Process terminated.');
+  });
+});
